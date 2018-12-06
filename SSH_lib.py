a2761350b259bd40f523a996b88320524c841a89
John Lampe <jlampe@tns4361l.local>
2018-12-05 20:42:35 -0500

    master: use this all the time

---
+++ SSH_lib.py
@@ -0,0 +1,100 @@
+import logging
+import os
+import re
+import pdb
+import json
+import sys, paramiko
+import SSH
+
+
+class SSH:
+
+    def __init__(self, CREDS):
+        self.hostname = CREDS['host']
+        self.port = CREDS['port']
+        self.user = CREDS['user']
+        self.password = CREDS['passwd']
+
+
+    def connect_to_ssh(self):
+        ret = False
+   
+        try:
+            self.client = paramiko.SSHClient()
+            self.client.load_system_host_keys()
+            self.client.set_missing_host_key_policy(paramiko.WarningPolicy)
+            self.client.connect(self.hostname, port=self.port, username=self.user, password=self.password)
+            ret = True
+
+        except:
+            logging.error("Failed to connect to {}".format(CREDS['host']))
+            return ret
+
+        return ret
+
+
+
+    def ssh_exec_cmd(self, cmd):
+        ret = None
+
+        try:
+            stdin, stdout, stderr = self.client.exec_command(cmd)
+            ret = stdout.read()
+
+        except:
+           logging.error("Failed to connect to execute command {}".format(cmd))
+
+        return ret
+
+
+
+
+    def sftp_get_file(self, local_file, remote_file):
+
+        ret = None
+   
+        if (len(local_file) <= 0) or (len(remote_file) <= 0):
+            logging.error("Insufficient parameters sent to sftp_get_file function. Requires client-object, localfile, and remotefile")
+            return ret
+
+        try:
+            ftp_client=self.client.open_sftp()
+            ftp_client.get(remote_file, local_file)
+            ret = 1
+
+        except:
+            logging.error("Failed to retrieve {}".format(remote_file))
+            ret = None
+
+        finally:
+            ftp_client.close()
+
+        return ret
+
+
+
+
+    def sftp_put_file(self, local_file,remote_file):
+
+        ret = None
+
+        if (len(local_file) <= 0) or (len(remote_file) <= 0):
+            logging.error("Insufficient parameters sent to sftp_get_file function. Requires client-object, localfile, and remotefile")
+            return ret
+    
+        try:
+            ftp_client=self.client.open_sftp()
+            ftp_client.put(local_file,remote_file)
+            ret = 1
+
+        except:
+            logging.error("Failed to upload {}".format(remote_file))
+            ret = None
+
+        finally:
+            ftp_client.close()
+
+        return ret
+
+
+
