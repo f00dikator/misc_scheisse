8c68455484fec1dc4b28efde1d5adbbc50ff86b4
John Lampe <dmitry.chan@gmail.com>
2022-03-31 19:26:22 -0400

    Create rolling_pcaps.sh

---
+++ rolling_pcaps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this stupid script just grabs a full pcap every 15 seconds and deletes out pcaps that are greater than a day old...
+# it will take up gigs of HD space...so...know that
+
+while :
+    do
+        for i in {1..240}
+            do echo $i ;
+            tcpdump -i enp2s0 -G 15 -W 1 -s0 -w $(date --date='-1 hour' +\%Y-\%m-\%d-\%s).pcap ;
+        done;
+        find . -name \*.pcap -type f -mtime +1 -delete;
+    done
+
+
